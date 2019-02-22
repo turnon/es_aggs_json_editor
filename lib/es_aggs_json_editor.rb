ad34f52b0b6a01b9302d60514ca5a1099ab5c463
ken <block24block@gmail.com>
2019-02-22 13:27:42 +0000

    init

---
+++ lib/es_aggs_json_editor.rb
@@ -0,0 +1,6 @@
+require "es_aggs_json_editor/version"
+
+module EsAggsJsonEditor
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require "es_aggs_json_editor"
+
+require "minitest/autorun"
